620541f56b44d17e6bf839810bb1e3f8311f234f
Dimos Mytel <dimos.mitel@gmail.com>
2020-04-25 12:59:04 +0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container">
+      <form class=" col-lg-6 offset-lg-3">
+        <div class="row justify-content-center" >
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" v-model="todo.item"
+            placeholder="Todo">
+             <div class="input-group-append">
+               <button class="btn btn-outline-secondary"
+                type="button" id="button-addon2" @click="addTodo">Add
+                </button>
+            </div>
+          </div>
+        </div>
+
+        <ul v-for="(todo, index) in todos" :key="todo">
+            <li>
+            <span v-if="isUpdate">{{todo}}</span>
+            <span v-else>
+              <input type="text" class="form-control" v-model="todo.item"
+              placeholder="Todo">
+            </span>
+            <button class="btn btn-outline-secondary delete"
+                type="button" id="button-addon3" @click="deleteTodo(index)">Delete
+            </button>
+            <button class="btn btn-outline-secondary update"
+                type="button" id="button-addon3" @click="updateTodo()">Update
+            </button>
+            </li>
+        </ul>
+      </form>
+
+        </div>
+   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      todos: [],
+      todo: [{
+        item: '',
+      }],
+      isUpdate: true,
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push(this.todo.item);
+    },
+    deleteTodo(i) {
+      this.todos.splice(i, 1);
+    },
+    updateTodo() {
+      this.isUpdate = false;
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
